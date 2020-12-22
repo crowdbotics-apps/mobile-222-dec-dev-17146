c6a1cdbb328df9989e7e94cfb2c5c132b2557c12
Crowdbotics <team@crowdbotics.com>
2020-12-22 08:22:14 +0000

    Update components

--- src/features/BlankScreen016064/screens/blank.js
+++ src/features/BlankScreen016064/screens/blank.js
@@ -24,12 +24,26 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { CheckBox_3: true, CheckBox_11: true }
+  state = {
+    Switch_17: true,
+    CheckBox_3: true,
+    CheckBox_23: true,
+    CheckBox_11: true,
+    Switch_15: true
+  }
 
   render = () => (
     <View>
       <View>
         <View>
+          <Switch
+            trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+            style={styles.Switch_17}
+            value={this.state.Switch_17}
+            onValueChange={nextChecked =>
+              this.setState({ Switch_17: nextChecked })
+            }
+          />
           <CheckBox
             title="Radio button"
             checkedIcon="dot-circle-o"
@@ -37,14 +51,34 @@ export default class Blank extends React.Component {
             checked={this.state.CheckBox_3}
             onPress={nextChecked => this.setState({ CheckBox_3: nextChecked })}
           />
+          <CheckBox
+            title="Checkbox"
+            checked={this.state.CheckBox_23}
+            onPress={nextChecked => this.setState({ CheckBox_23: nextChecked })}
+          />
         </View>
-        <View>
+        <ImageBackground
+          source={{
+            uri:
+              "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/file_example_JPG_100kB_E8y4rYP.jpg"
+          }}
+          style={styles.ImageBackground_9}
+        >
           <CheckBox
             title="Checkbox"
             checked={this.state.CheckBox_11}
             onPress={nextChecked => this.setState({ CheckBox_11: nextChecked })}
           />
-        </View>
+          <Switch
+            trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+            style={styles.Switch_15}
+            value={this.state.Switch_15}
+            onValueChange={nextChecked =>
+              this.setState({ Switch_15: nextChecked })
+            }
+          />
+          <Icon name="star" />
+        </ImageBackground>
       </View>
     </View>
   )
@@ -54,7 +88,11 @@ const styles = StyleSheet.create({
   View_1: {},
   View_7: {},
   View_8: {},
+  Switch_17: { alignSelf: "flex-start" },
   CheckBox_3: {},
-  View_9: {},
-  CheckBox_11: {}
+  CheckBox_23: {},
+  ImageBackground_9: { backgroundSize: "stretch" },
+  CheckBox_11: {},
+  Switch_15: { alignSelf: "flex-start" },
+  Icon_46: {}
 })
